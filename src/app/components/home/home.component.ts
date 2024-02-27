b9b988826a6f77ff759eb6b776def89280434753
Verda Muso <gearmobile@gmail.com>
2024-02-27 22:07:44 +0300

    cleanup home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,6 @@ import { HideButtonAction, ShowButtonAction } from '../../../state/actions';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
